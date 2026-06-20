c9a094ccb5a57801f0c6022374fc63f23678d31e
micelele1 <michelleyeo36@gmail.com>
2026-06-20 22:20:52 +0700

    nambahin comment
    Added a comment block describing the main.c file and its functionality.

--- TubesPMP/src/main.c
+++ TubesPMP/src/main.c
@@ -1,3 +1,9 @@
+/*
+Tugas Besar PMP K02 Kelompok 01
+Nama file/function: main.c
+Deskripsi file/function: File utama yang mengintegrasikan inisialisasi modul UART, database inventaris, dan antarmuka pengguna, serta menjalankan loop utama program mikrokontroler.
+*/
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "../include/inventory_db.h"
@@ -5,13 +11,13 @@
 #include "../include/interface.h"
 
 int main(void) {
-uart_init(9600);
-initDB();
-interface_init();
+    uart_init(9600);
+    initDB();
+    interface_init();
 
-while (1) {
-    interface_loop();
-}
+    while (1) {
+        interface_loop();
+    }
 
-return 0;
+    return 0;
 }
